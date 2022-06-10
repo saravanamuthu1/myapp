23f069b8cb3cc4a34e34c00dae9f921aab35b56c
saravanamuthu <saravanamuthu.97@gmail.com>
2022-06-10 16:03:32 -0500

    deleted some nav elements

--- src/Component/Navfunction.js
+++ src/Component/Navfunction.js
@@ -20,12 +20,6 @@ function Navfunction() {
             <Nav.Link as = {Link} to= {"/Project"}>Project</Nav.Link>
             <Nav.Link as = {Link} to= {"/Publications"}>Publication</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
